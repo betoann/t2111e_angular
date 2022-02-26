9ea4ea4330916fc4976a23b672c9af0d9aecbefc
betoann <toandgkthd12@gmail.com>
2022-02-26 20:23:25 +0700

    angular assignment1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {ForgotpasswordComponent} from "./forgotpassword/forgotpassword.component";
+import {RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
+const appRoutes:Routes = [
+  {path:"",component:LoginComponent},
+  {path:"register",component:RegisterComponent},
+  {path:"forgot-password",component:ForgotpasswordComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,FooterComponent,
+    LoginComponent,RegisterComponent,ForgotpasswordComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
