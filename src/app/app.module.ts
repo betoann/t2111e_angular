7c3e56497f438b0c0fab140f45840697df6b3262
betoann <toandgkthd12@gmail.com>
2022-03-10 22:36:55 +0700

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,26 @@ import {RegisterComponent} from "./register/register.component";
 import {ForgotpasswordComponent} from "./forgotpassword/forgotpassword.component";
 import {RouterModule, Routes} from "@angular/router";
 import {FormsModule} from "@angular/forms";
+import {CurrentWeatherComponent} from "./currentweather/currentweather.component";
+import {HttpClientModule} from "@angular/common/http";
+import {ForecastComponent} from "./forecastweather/forecast.component";
 
 const appRoutes:Routes = [
   {path:"",component:LoginComponent},
   {path:"register",component:RegisterComponent},
   {path:"forgot-password",component:ForgotpasswordComponent},
+  {path:"current-weather",component:CurrentWeatherComponent},
+  {path:"forecast-weather",component:ForecastComponent}
 ]
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,FooterComponent,
-    LoginComponent,RegisterComponent,ForgotpasswordComponent
+    LoginComponent,RegisterComponent,ForgotpasswordComponent,
+    CurrentWeatherComponent,ForecastComponent
   ],
   imports: [
-    BrowserModule,FormsModule,
+    BrowserModule,FormsModule,HttpClientModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
